3b1959511144f34da68cccf70d4d39651090a2b4
Marcin Przepiorowski <marcin@delphix.com>
2019-12-05 15:02:17 +0000

    test

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
-pwd 
-ls -l 
-ls -l /github/workspace 
+
+mkdir /github/workspace/out
+
+source scl_source enable rh-perl526 
+cd /github/workspace/bin
+pp -u -I /github/workspace/lib -M Text::CSV_PP -M List::MoreUtils::PP -M Crypt::Blowfish  \
+       -F Crypto=dbutils\.pm$ -M Filter::Crypto::Decrypt -o /github/workspace/out/dxtoolkit `ls dx_*.pl | xargs`
+
+for i in dx_*.pl ; do name=${i%.pl}; ln -s /github/workspace/out/dxtoolkit /github/workspace/out/$name; done
 
