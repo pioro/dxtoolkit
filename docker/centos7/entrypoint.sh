6c1ab7ba2a57d992c7e47f153b3997ab583f9ea4
Marcin Przepiorowski <marcin@delphix.com>
2019-12-31 16:10:33 +0000

    test

--- docker/centos7/entrypoint.sh
+++ docker/centos7/entrypoint.sh
@@ -1,9 +1,24 @@
 #!/bin/bash
 
+# TODO: replace encryption key 
+
 export DELPHIX_OUTPUT=/github/workspace/dxtoolkit2
+export DXTOOLKIT_CONF=/github/workspace/test/dxtools.conf
 mkdir $DELPHIX_OUTPUT
 
 source scl_source enable rh-perl526 
+
+echo "run tests"
+
+cd /github/workspace/test
+chmod +x ./runtest.sh
+./runtest.sh
+
+if [[ $? -ne 0 ]]; then
+    exit 1;
+fi
+
+
 cd /github/workspace/bin
 pp -u -I /github/workspace/lib -M Text::CSV_PP -M List::MoreUtils::PP -M Crypt::Blowfish  \
       -F Crypto=dbutils\.pm$ -M Filter::Crypto::Decrypt -o $DELPHIX_OUTPUT/dxtoolkit `ls dx_*.pl | xargs`
