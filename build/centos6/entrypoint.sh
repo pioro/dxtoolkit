b33eb3441058a86fbdd047b988ddbf82abe4a1c7
Marcin Przepiorowski <marcin@delphix.com>
2020-01-03 16:12:56 +0000

    test

--- build/centos6/entrypoint.sh
+++ build/centos6/entrypoint.sh
@@ -8,12 +8,14 @@ source scl_source enable rh-perl524
 cd /github/workspace/lib
 mv dbutils.pm dbutils.orig.pm
 cat dbutils.orig.pm | sed -e "s/put your encryption key here/${INPUT_ENCKEY}/" > dbutils.pm
+ls -l dbutils*
 
 cd /github/workspace/bin
 pp -u -I /github/workspace/lib -M Text::CSV_PP -M List::MoreUtils::PP -M Crypt::Blowfish  \
-      -F Crypto=dbutils\.pm$ -M Filter::Crypto::Decrypt -o $DELPHIX_OUTPUT/dxtoolkit `ls dx_*.pl | xargs`
+      -F Crypto=dbutils\.pm$ -M Filter::Crypto::Decrypt -o $DELPHIX_OUTPUT/runner `ls dx_*.pl | xargs`
 
-for i in dx_*.pl ; do name=${i%.pl}; ln -s $DELPHIX_OUTPUT/dxtoolkit $DELPHIX_OUTPUT/$name; done
+cd $DELPHIX_OUTPUT
+for i in /github/workspace/bin/dx_*.pl ; do name=`basename -s .pl $i`; ln -s runner $name; done
 
 cd /github/workspace
 tar czvf /github/workspace/dxtoolkit.tar.gz dxtoolkit2/
